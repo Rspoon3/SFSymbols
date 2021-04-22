aefd8037e6226fe06929b7a7009f70b176a978ed
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-04-22 17:01:07 -0400

    • Category now has human readable raw values • Category conforms to Hashable

--- Sources/SFSymbols/SFSymbol+Category.swift
+++ Sources/SFSymbols/SFSymbol+Category.swift
@@ -8,30 +8,30 @@
 import Foundation
 
 public extension SFSymbol{
-    enum Category: String, CaseIterable {
-        case all
-        case whatsNew
-        case multiColor
-        case communication
-        case weather
-        case objectsAndTools
-        case devices
-        case gaming
-        case connectivity
-        case transportation
-        case human
-        case nature
-        case editing
-        case textFormatting
-        case media
-        case keyboard
-        case commerce
-        case time
-        case health
-        case shapes
-        case arrows
-        case indices
-        case math
+    enum Category: String, CaseIterable, Hashable{
+        case all             = "All"
+        case whatsNew        = "What's New"
+        case multiColor      = "Multicolor"
+        case communication   = "Communication"
+        case weather         = "Weather"
+        case objectsAndTools = "Objects & Tools"
+        case devices         = "Devices"
+        case gaming          = "Gaming"
+        case connectivity    = "Connectivity"
+        case transportation  = "Transportation"
+        case human           = "Human"
+        case nature          = "Nature"
+        case editing         = "Editing"
+        case textFormatting  = "Text Formatting"
+        case media           = "Media"
+        case keyboard        = "Keyboard"
+        case commerce        = "Commerce"
+        case time            = "Time"
+        case health          = "Health"
+        case shapes          = "Shapes"
+        case arrows          = "Arrows"
+        case indices         = "Indices"
+        case math            = "Math"
         
         public var symbols: [SFSymbol]{
             return SFSymbol.allSymbols.filter({$0.categories.contains(self)})
