5f5b1d98d686d0882fc4178e3bfcc3e25b6e1516
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-04-22 17:48:27 -0400

    • Added a symbol for each Category. 	-Mainly to be used in Labels

--- Sources/SFSymbols/SFSymbol+Category.swift
+++ Sources/SFSymbols/SFSymbol+Category.swift
@@ -36,5 +36,68 @@ public extension SFSymbol{
         public var symbols: [SFSymbol]{
             return SFSymbol.allSymbols.filter({$0.categories.contains(self)})
         }
+        
+        public var symbol: SFSymbol{
+            switch self {
+            case .all:
+                return .squareGrid2X2
+            case .whatsNew:
+                return .sparkles
+            case .multiColor:
+                if #available(iOS 14, *) {
+                    return .paintpalette
+                } else{
+                    return .eyedropper
+                }
+            case .communication:
+                return .message
+            case .weather:
+                return .cloudSun
+            case .objectsAndTools:
+                return .folder
+            case .devices:
+                return .desktopcomputer
+            case .gaming:
+                return .gamecontroller
+            case .connectivity:
+                return .antennaRadiowavesLeftAndRight
+            case .transportation:
+                return .carFill
+            case .human:
+                return .personCropCircle
+            case .nature:
+                if #available(iOS 14, *) {
+                    return .leaf
+                } else{
+                    return .flame
+                }
+            case .editing:
+                return .sliderHorizontal3
+            case .textFormatting:
+                return .textformat
+            case .media:
+                return .playpause
+            case .keyboard:
+                return .command
+            case .commerce:
+                return .cart
+            case .time:
+                return .timer
+            case .health:
+                return .staroflife
+            case .shapes:
+                return .squareOnCircle
+            case .arrows:
+                if #available(iOS 14, *) {
+                    return .arrowForward
+                } else{
+                    return .arrowUturnLeft
+                }
+            case .indices:
+                return .aCircle
+            case .math:
+                return .xSquareroot
+            }
+        }
     }
 }
