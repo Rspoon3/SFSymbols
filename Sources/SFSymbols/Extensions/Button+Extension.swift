7686e6009d8807eac9db19d7afdeb01663785d42
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-22 17:20:39 -0500

    • Updated Button extension

--- Sources/SFSymbols/Extensions/Button+Extension.swift
+++ Sources/SFSymbols/Extensions/Button+Extension.swift
@@ -14,32 +14,43 @@ extension Button where Label == Image {
             Image(systemName: symbol.title)
         }
     }
+    
+    @available(iOS 15, macOS 15.0, tvOS 15.0, watchOS 8.0,  *)
+    public init(symbol: SFSymbol, role: ButtonRole, action: @escaping () -> Void) {
+        self.init(role: role, action: action) {
+            Image(systemName: symbol.title)
+        }
+    }
 }
+
 
 
 @available(iOS 14, macOS 14.0, tvOS 14.0, watchOS 7.0,  *)
 extension Button where Label == SwiftUI.Label<Text, Image>{
     public init(_ titleKey: LocalizedStringKey, symbol: SFSymbol, textColor: Color? = nil, action: @escaping () -> Void) {
-        self.init(action: action, label: {
-            SwiftUI.Label(
-                title: {
-                    Text(titleKey)
-                        .foregroundColor(textColor)
-                },
-                icon: {
-                    Image(symbol: symbol)
-                }
-            )
-        })
+        self.init {
+            action()
+        } label: {
+            Label {
+                Text(titleKey)
+                    .foregroundColor(textColor)
+            } icon: {
+                Image(systemName: symbol.title)
+            }
+        }
     }
-}
-
-
-@available(iOS 15, macOS 15.0, tvOS 15.0, watchOS 8.0,  *)
-extension Button where Label == Image {
-    public init(symbol: SFSymbol, role: ButtonRole, action: @escaping () -> Void) {
-        self.init(role: role, action: action) {
-            Image(systemName: symbol.title)
+    
+    @available(iOS 15, macOS 15.0, tvOS 15.0, watchOS 8.0,  *)
+    public init(_ titleKey: LocalizedStringKey,  symbol: SFSymbol, role: ButtonRole? = nil, textColor: Color? = nil, action: @escaping () -> Void) {
+        self.init(role: role) {
+            action()
+        } label: {
+            Label {
+                Text(titleKey)
+                    .foregroundColor(textColor)
+            } icon: {
+                Image(systemName: symbol.title)
+            }
         }
     }
 }
