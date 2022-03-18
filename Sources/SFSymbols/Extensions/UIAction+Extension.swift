ee6ad6d8f83e7747e08617963a31a4f8e497dd69
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-03-18 15:40:59 -0400

    Added UIAction initializer

---
+++ Sources/SFSymbols/Extensions/UIAction+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIAction+Extension.swift
+//  
+//
+//  Created by Richard Witherspoon on 3/18/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIAction{
+    public convenience init(title: String = "",
+                            symbol: SFSymbol,
+                            identifier: UIAction.Identifier? = nil,
+                            discoverabilityTitle: String? = nil,
+                            attributes: UIMenuElement.Attributes = [],
+                            state: UIMenuElement.State = .off,
+                            handler: @escaping UIActionHandler){
+        self.init(title: title,
+                  image: UIImage(symbol: symbol),
+                  identifier: identifier,
+                  discoverabilityTitle: discoverabilityTitle,
+                  attributes: attributes,
+                  state: state,
+                  handler: handler)
+    }
+    
+    @available(iOS 15.0, tvOS 15.0, *)
+    public convenience init(title: String = "",
+                            subtitle: String? = nil,
+                            symbol: SFSymbol,
+                            identifier: UIAction.Identifier? = nil,
+                            discoverabilityTitle: String? = nil,
+                            attributes: UIMenuElement.Attributes = [],
+                            state: UIMenuElement.State = .off,
+                            handler: @escaping UIActionHandler){
+        self.init(title: title,
+                  subtitle: subtitle,
+                  image: UIImage(symbol: symbol),
+                  identifier: identifier,
+                  discoverabilityTitle: discoverabilityTitle,
+                  attributes: attributes,
+                  state: state,
+                  handler: handler)
+    }
+}
