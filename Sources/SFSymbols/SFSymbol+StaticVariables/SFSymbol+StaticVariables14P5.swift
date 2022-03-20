ce4b7482b43362f86b4ad6e4847dec5ad0f873fb
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-03-20 10:10:56 -0400

    Updated to uses multiple static variable files

---
+++ Sources/SFSymbols/SFSymbol+StaticVariables/SFSymbol+StaticVariables14P5.swift
@@ -0,0 +1,159 @@
+import Foundation
+
+@available(iOS 14.5, macOS 11.3, tvOS 14.5, watchOS 7.4, *)
+public extension SFSymbol {
+    static let airpodsmax = SFSymbol(title: "airpodsmax",
+                                        categories: [.devices, .objectsandtools],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let applewatchSideRight = SFSymbol(title: "applewatch.side.right",
+                                        categories: [.devices],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubble = SFSymbol(title: "character.bubble",
+                                        categories: [.communication],
+                                        searchTerms: ["a"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubbleAr = SFSymbol(title: "character.bubble.ar",
+                                        categories: [.communication],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubbleFill = SFSymbol(title: "character.bubble.fill",
+                                        categories: [.communication],
+                                        searchTerms: ["a"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubbleFillAr = SFSymbol(title: "character.bubble.fill.ar",
+                                        categories: [.communication],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubbleFillHe = SFSymbol(title: "character.bubble.fill.he",
+                                        categories: [.communication],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterBubbleHe = SFSymbol(title: "character.bubble.he",
+                                        categories: [.communication],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeam = SFSymbol(title: "character.cursor.ibeam",
+                                        categories: [.textformatting],
+                                        searchTerms: ["a", "insert.text"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamAr = SFSymbol(title: "character.cursor.ibeam.ar",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamHe = SFSymbol(title: "character.cursor.ibeam.he",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamHi = SFSymbol(title: "character.cursor.ibeam.hi",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamJa = SFSymbol(title: "character.cursor.ibeam.ja",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamKo = SFSymbol(title: "character.cursor.ibeam.ko",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterCursorIbeamTh = SFSymbol(title: "character.cursor.ibeam.th",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextbox = SFSymbol(title: "character.textbox",
+                                        categories: [.textformatting],
+                                        searchTerms: ["a", "insert.textbox"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxAr = SFSymbol(title: "character.textbox.ar",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxHe = SFSymbol(title: "character.textbox.he",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxHi = SFSymbol(title: "character.textbox.hi",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxJa = SFSymbol(title: "character.textbox.ja",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxKo = SFSymbol(title: "character.textbox.ko",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let characterTextboxTh = SFSymbol(title: "character.textbox.th",
+                                        categories: [.textformatting],
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let hifispeakerAndHomepodmini = SFSymbol(title: "hifispeaker.and.homepodmini",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let hifispeakerAndHomepodminiFill = SFSymbol(title: "hifispeaker.and.homepodmini.fill",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodAndHomepodmini = SFSymbol(title: "homepod.and.homepodmini",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodAndHomepodminiFill = SFSymbol(title: "homepod.and.homepodmini.fill",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodmini = SFSymbol(title: "homepodmini",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodmini2 = SFSymbol(title: "homepodmini.2",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodmini2Fill = SFSymbol(title: "homepodmini.2.fill",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let homepodminiFill = SFSymbol(title: "homepodmini.fill",
+                                        categories: [.devices],
+                                        searchTerms: ["audio", "sound", "speaker"],
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+
+    static let rectangleTopthirdInsetFill = SFSymbol(title: "rectangle.topthird.inset.fill",
+                                        categories: nil,
+                                        searchTerms: nil,
+                                        releaseInfo: ReleaseInfo(iOS: 14.5, macOS: 11.3, tvOS: 14.5, watchOS: 7.4))
+}
