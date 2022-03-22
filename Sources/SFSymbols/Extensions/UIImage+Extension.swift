8a921c120cc14d924d1d176be1e4243ae0916951
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-03-22 11:21:26 -0400

    Added additional UIKit extensions

--- Sources/SFSymbols/Extensions/UIImage+Extension.swift
+++ Sources/SFSymbols/Extensions/UIImage+Extension.swift
@@ -8,9 +8,22 @@
 #if canImport(UIKit)
 import UIKit
 
-extension UIImage {
-    public convenience init(symbol: SFSymbol){
+public extension UIImage {
+    convenience init(symbol: SFSymbol){
         self.init(systemName: symbol.title)!
+    }
+    
+    convenience init(symbol: SFSymbol, withConfiguration configuration: UIImage.Configuration?){
+        self.init(systemName: symbol.title, withConfiguration: configuration)!
+    }
+    
+    convenience init(symbol: SFSymbol, weight: UIImage.SymbolWeight){
+        let configuration = UIImage.SymbolConfiguration(weight: weight)
+        self.init(systemName: symbol.title, withConfiguration: configuration)!
+    }
+    
+    convenience init(symbol: SFSymbol, compatibleWith traitCollection: UITraitCollection?){
+        self.init(systemName: symbol.title, compatibleWith: traitCollection)!
     }
 }
 #endif
