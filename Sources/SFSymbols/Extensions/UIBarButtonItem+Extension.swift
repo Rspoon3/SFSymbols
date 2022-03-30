0d77cf88b14802d61a9bc152551c09ba12f5c9cc
Ricky <42879920+Rspoon3@users.noreply.github.com>
2022-03-30 10:48:54 -0400

    Added UIBarButtonItem extension

---
+++ Sources/SFSymbols/Extensions/UIBarButtonItem+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIBarButtonItem+Extension.swift
+//  
+//
+//  Created by Richard Witherspoon on 3/30/22.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+extension UIBarButtonItem {
+    public convenience init(symbol: SFSymbol, landscapeImagePhone: UIImage?, style: UIBarButtonItem.Style, target: Any?, action: Selector?){
+        self.init(image: UIImage(symbol: symbol), landscapeImagePhone: landscapeImagePhone, style: style, target: target, action: action)
+    }
+
+    public convenience init(symbol: SFSymbol, style: UIBarButtonItem.Style, target: Any?, action: Selector?){
+        self.init(image: UIImage(symbol: symbol), style: style, target: target, action: action)
+
+    }
+    
+    public convenience init(title: String? = nil, symbol: SFSymbol, primaryAction: UIAction? = nil, menu: UIMenu? = nil){
+        self.init(title: title, image: UIImage(symbol: symbol), primaryAction: primaryAction, menu: menu)
+    }
+}
+#endif
