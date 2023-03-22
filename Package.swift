d72c55203d3b0c9f1f380d37cd7112e399fb671f
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-03-22 01:19:57 -0400

    excluding preview app

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SFSymbols",
-            dependencies: []),
+            exclude: ["SFSymbolsPreview"]
+        ),
         .testTarget(
             name: "SFSymbolsTests",
             dependencies: ["SFSymbols"]),
