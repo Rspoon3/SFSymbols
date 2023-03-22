9d87d3ad5347b4ff84f42d183aa79c52dd94a8e3
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2023-03-22 01:52:14 -0400

    Updating demo app

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SFSymbols",
-            exclude: ["SFSymbolsPreview"]
+            name: "SFSymbols"
         ),
         .testTarget(
             name: "SFSymbolsTests",
---
+++ SFSymbolsDemo/SFSymbolsDemo/SFSymbolsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsDemoApp.swift
+//  SFSymbolsDemo
+//
+//  Created by Richard Witherspoon on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
