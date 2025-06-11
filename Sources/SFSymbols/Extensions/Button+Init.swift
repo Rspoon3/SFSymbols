fc13d0a74318c9cbdd3a347f74f7dd5e6571f3e1
Ricky Witherspoon <42879920+Rspoon3@users.noreply.github.com>
2025-06-11 16:24:00 -0400

    compile fixes

--- Sources/SFSymbols/Extensions/Button+Init.swift
+++ Sources/SFSymbols/Extensions/Button+Init.swift
@@ -24,6 +24,52 @@ public extension Button where Label == Image {
     }
 }
 
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
+public extension Button where Label == SwiftUI.Label<Text, Image>{
+    /// Creates a button with a specified role that generates its label from a localized string key and an `SFSymbol`.
+    ///
+    /// - Parameters
+    ///     - titleKey: The key for the button’s localized title, that describes the purpose of the button’s action
+    ///     - symbol: The `SFSymbol` describing this image.
+    ///     - role: An optional semantic role describing the button. A value of nil means that the button doesn’t have an assigned role.
+    ///     - action: The action to perform when the user triggers the button.
+    init(
+        _ titleKey: LocalizedStringKey,
+        symbol: SFSymbol,
+        role: ButtonRole?,
+        action: @escaping () -> Void
+    ) {
+        self.init(
+            titleKey,
+            systemImage: symbol.title,
+            role: role,
+            action: action
+        )
+    }
+    
+    /// Creates a button with a specified role that generates its label from a string and an `SFSymbol`.
+    ///
+    /// - Parameters
+    ///     - title: A string that describes the purpose of the button’s action.
+    ///     - symbol: The `SFSymbol` describing this image.
+    ///     - role: An optional semantic role describing the button. A value of nil means that the button doesn’t have an assigned role.
+    ///     - action: The action to perform when the user triggers the button.
+    @_disfavoredOverload
+    init<S>(
+        _ title: S,
+        symbol: SFSymbol,
+        role: ButtonRole?,
+        action: @escaping () -> Void
+    ) where S : StringProtocol {
+        self.init(
+            title,
+            systemImage: symbol.title,
+            role: role,
+            action: action
+        )
+    }
+}
+
 @available(iOS 14, macOS 14.0, tvOS 14.0, watchOS 7.0,  *)
 public extension Button where Label == SwiftUI.Label<Text, Image>{
     init(
@@ -97,51 +143,6 @@ public extension Button where Label == SwiftUI.Label<Text, Image>{
         self.init(
             titleKey,
             systemImage: symbol.title,
-            action: action
-        )
-    }
-    
-    /// Creates a button with a specified role that generates its label from a string and an `SFSymbol`.
-    ///
-    /// - Parameters
-    ///     - title: A string that describes the purpose of the button’s action.
-    ///     - symbol: The `SFSymbol` describing this image.
-    ///     - role: An optional semantic role describing the button. A value of nil means that the button doesn’t have an assigned role.
-    ///     - action: The action to perform when the user triggers the button.
-    @_disfavoredOverload
-    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-    init<S>(
-        _ title: S,
-        symbol: SFSymbol,
-        role: ButtonRole?,
-        action: @escaping () -> Void
-    ) where S : StringProtocol {
-        self.init(
-            title,
-            systemImage: symbol.title,
-            role: role,
-            action: action
-        )
-    }
-    
-    /// Creates a button with a specified role that generates its label from a localized string key and an `SFSymbol`.
-    ///
-    /// - Parameters
-    ///     - titleKey: The key for the button’s localized title, that describes the purpose of the button’s action
-    ///     - symbol: The `SFSymbol` describing this image.
-    ///     - role: An optional semantic role describing the button. A value of nil means that the button doesn’t have an assigned role.
-    ///     - action: The action to perform when the user triggers the button.
-    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-    init(
-        _ titleKey: LocalizedStringKey,
-        symbol: SFSymbol,
-        role: ButtonRole?,
-        action: @escaping () -> Void
-    ) {
-        self.init(
-            titleKey,
-            systemImage: symbol.title,
-            role: role,
             action: action
         )
     }
